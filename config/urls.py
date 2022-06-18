42e3eff4c6accf64d76e1fc335a938445a1f0e28
Eva-Kravets <71512957+Eva-Kravets@users.noreply.github.com>
2022-06-18 08:39:44 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,11 @@ router.register('ingredient', IngredientViewSet)
 router.register('calories', CaloriesViewSet)
 router.register('user', UserViewSet)
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/recipes-filter/', RecipeFilters.as_view()),
